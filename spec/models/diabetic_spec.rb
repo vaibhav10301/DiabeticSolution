8efd0d88f68cdd7c188f52436484f32fc68ad81c
vaibhav10301 <agrawal10301@gmail.com>
2016-07-26 00:32:59 +0530

    test cases

---
+++ spec/models/diabetic_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Diabetic  do
+
+  it "is single digit integer value" do 
+  	diabetic = Diabetic.new(glucose_level: 10)
+  	expect(diabetic).not_to be_valid
+  end
+
+  it "should not accept float value" do 
+  	diabetic = Diabetic.new(glucose_level: 1.12)
+  	expect(diabetic).not_to be_valid
+  end
+
+  it "should not accept any character"	do 
+  	diabetic = Diabetic.new(glucose_level: "aa")
+  	expect(diabetic).not_to be_valid
+  end
+
+  it "should not accept any character"	do 
+    diabetic = Diabetic.new(glucose_level: 5)
+  	expect(diabetic).to be_valid
+  end
+end
